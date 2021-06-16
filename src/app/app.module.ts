48535fe93b2bced8f8017ff93b9464822200d978
Karina Yevenes <karina.yevenes.hernandez@gmail.com>
2021-06-16 12:45:44 -0400

    cambios que no me esperaba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarRightComponent } from './shared/sidebar-right/sidebar-right.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    SidebarRightComponent,
+    FooterComponent,
+    ProgressComponent,
+    PagesComponent,
+    Grafica1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/sidebar-right/sidebar-right.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-right',
+  templateUrl: './sidebar-right.component.html'
+})
+export class SidebarRightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
